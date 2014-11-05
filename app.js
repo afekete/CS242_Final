9d0c540e2e01a935b2a35bf69e442aef602e596c
Alec Fekete <fekete2@illinois.edu>
2014-11-05 16:38:25 -0600

    Added comments to app.js Added favicon

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan'); //HTTP request logger middleware for node.js
+var cookieParser = require('cookie-parser'); //Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
+var bodyParser = require('body-parser'); //Node.js body parsing middleware.
 
+// Store the routes in vars to be used by express
 var index = require('./routes/index');
-var users = require('./routes/users');
 
+// Create the express web app
 var app = express();
 
-// view engine setup
+// Set the relative path of the views and set the view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// Set up all the middleware. Each server request will go through all the middleware
+// and run any that matches the request path
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
