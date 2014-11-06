55386e6a372a338dd12a6349886b3754d5b8f056
Alec Fekete <fekete2@illinois.edu>
2014-11-06 11:09:08 -0600

    Fixed app.use for main

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', [index, db]);
-app.use('/main',[main, db]);
+app.use('/', [index, main, db]);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
