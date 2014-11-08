9bcfc373d7e39ebcad1a39cf9d637a849368b8d2
kavyagundavaram <gundava2@illinois.edu>
2014-11-08 17:51:49 -0600

    it works better now

--- public/js/instagram.js
+++ public/js/instagram.js
@@ -1,30 +1,70 @@
-/**
- * Created by Alec on 11/8/2014.
- */
-$(document).ready(function() {
+
+var global_next_url = ""
+var counter = 0
+$(document).ready(function(){
     $( "#tag_input" ).submit(function( event ) {
         event.preventDefault();
         var given_tag = $( "#tag_input .form-group .form-control" ).val()
         getAndAddPictures(given_tag, 100)
     });
+    $(window).scroll(function(){
+        if ($(window).scrollTop()==$(document).height() - $(window).height()){
+            if(global_next_url != "")
+            {
+                addNextPicture(global_next_url)
+            }
+        }
+    });
+
 });
 
-function getAndAddPictures(hash, count) {
-    $('.pattern').on('willLoadInstagram', function(event, options) {
-        console.log(options);
-    });
-    $('.pattern').on('didLoadInstagram', function(event, response) {
-        console.log(response);
-        $('#pattern ul').empty()
-        response.data.forEach(function(picture, index) {
-            $('#pattern ul').append(
-                '<li><a href="#"><img src="'+picture.images.standard_resolution.url+'"></a></li>'
-            )
-        })
-    });
-    $('.pattern').instagram({
-        hash: hash,
-        clientId: '93cfcf70cba44318a06a07ea8e3b6268',
-        count: count
+function getAndAddPictures(tag, count) {
+    var access_token = "394307472.93cfcf7.cc10311c67174728a0baef44810d5c0c";
+    //user_id_kavya = 394307472
+
+    var tag_endpoint = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?count=" + count + "&access_token=" + access_token;
+    var popular_endpoint = "https://api.instagram.com/v1/media/popular?count=" + count + "access_token=" + access_token;
+    //var user_endpoint = "https://api.instagram.com/v1/users/" + user_id_kavya + "/media/recent?access_token=" + access_token;
+
+    $.ajax({
+
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: tag_endpoint,
+        success: function (data) {
+            $('#pattern ul').empty()
+            data.data.forEach(function (picture, index) {
+                $('#pattern ul').append(
+                    '<li><a href="#"><img src="' + picture.images.standard_resolution.url + '"></a></li>'
+                )
+            })
+            global_next_url = data.pagination.next_url
+            counter++
+            console.log(counter)
+        }
     });
 }
+
+function addNextPicture(next_url){
+
+    $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: next_url,
+        success: function (data) {
+            data.data.forEach(function (picture, index) {
+                $('#pattern ul').append(
+                    '<li><a href="#"><img src="' + picture.images.standard_resolution.url + '"></a></li>'
+                )
+            })
+            global_next_url = data.pagination.next_url
+            counter++
+            console.log(counter)
+        }
+    });
+}
+
+
+
