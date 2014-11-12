2059575ddb3408a3a2a59cafce7893de7cdfad31
Alec Fekete <fekete2@illinois.edu>
2014-11-11 20:17:11 -0600

    Pretended to add post support

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,9 @@ router.get('/main', function(req, res) {
     res.render('main');
 });
 
+router.post('/main', function(req, res) {
+    res.send(req.data);
+    res.render('main');
+});
+
 module.exports = router;
