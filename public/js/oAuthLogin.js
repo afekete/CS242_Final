a902bde7f8c267d3c40636e537849dd7b93b8232
kavyagundavaram <gundava2@illinois.edu>
2014-11-13 20:13:42 -0600

    fixed how we are doing the oauth

---
+++ public/js/oAuthLogin.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $( "#user_input" ).submit(function( event ) {
+        event.preventDefault();
+        var given_user_name = $( "#tag_input .form-group .form-control" ).val()
+    });
+
+});
