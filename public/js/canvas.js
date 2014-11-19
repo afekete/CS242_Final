445743d5cbb5f479680c5232cb736f6f2467f91d
Robert <bartimious@gmail.com>
2014-11-18 19:52:48 -0600

    can add mosaics to db now

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -29,18 +29,22 @@ function iterate_canvas(possiblePictures) {
         }
     }
 }
-/*
+
 $(document).ready(function() {
-
-    for (var x =0;x < 16 ; x++) {
-        for (var y =0;y < 16 ; y++) {
-            var c = document.getElementById("main_canvas"+"_"+x+"_"+y);
-            var ctx = c.getContext("2d");
-            ctx.fillStyle = "#88"+ ((x*10)+20).toString(16)+ ((y*10)+20).toString(16);
-            ctx.fillRect(0, 0, 40, 40);
+    $("button").click(function(){
+        var newMosaic={
+            '_id' : $( "input" ).val(),
+            'tag' : localStorage.getItem("chosenTag"),
+            'colors' : localStorage.getItem("chosenPictureKey")
         }
-    }
+        $.ajax({
+            url: "/db/saved",
+            type: "POST",
+            data: newMosaic,
+            dataType: "JSON"
+        }).done(function( msg ) {
+            console.log(msg);
+        });
+    })
 
-    iterate_canvas()
 })
-*/
