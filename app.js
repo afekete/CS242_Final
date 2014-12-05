76c52dd34dc8e048adc3dc2b46b23afb20e51ecc
Robert <bartimious@gmail.com>
2014-12-04 20:45:41 -0600

    added location tab

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var mosaic = require('./routes/mosaic');
 var user = require('./routes/user');
 var signin = require('./routes/signin');
 var register = require('./routes/register')
+var location = require('./routes/location')
 
 // Create the express web app
 var app = express();
@@ -32,7 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.use('/', [index, main, contact, mosaic, user ,signin, register, db]);
+app.use('/', [index, main, contact, mosaic, user ,signin, register, location, db]);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
